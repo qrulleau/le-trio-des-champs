a9513b8fac50417f7a6f03dee9240f5df48cb63f
qrulleau <quentin.rulleau@yahoo.com>
2026-05-12 16:27:52 +0200

    feat: add subscribers admin page with CRUD

--- front/src/app/features/admin/pages/subscribers/subscribers.component.ts
+++ front/src/app/features/admin/pages/subscribers/subscribers.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { ApiService } from '../../../../core/services/api.service'
 
 @Component({
-  selector: 'app-subscriberscomponent',
+  selector: 'app-subscribers',
   standalone: true,
   imports: [CommonModule],
-  template: '<p>SubscribersComponent works</p>',
+  templateUrl: './subscribers.component.html',
 })
-export class SubscribersComponent {}
+export class SubscribersComponent implements OnInit {
+  subscribers: any[] = []
+  subscribersByCity: { [key: string]: any[] } = {}
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.loadSubscribers()
+  }
+
+  loadSubscribers() {
+    this.api.getSubscribers().subscribe((data) => {
+      this.subscribers = data
+      this.groupByCity()
+    })
+  }
+
+  groupByCity() {
+    this.subscribersByCity = {}
+    for (const subscriber of this.subscribers) {
+      for (const city of subscriber.cities || []) {
+        if (!this.subscribersByCity[city.name]) {
+          this.subscribersByCity[city.name] = []
+        }
+        this.subscribersByCity[city.name].push(subscriber)
+      }
+    }
+  }
+
+  getCityNames(): string[] {
+    return Object.keys(this.subscribersByCity)
+  }
+
+  delete(id: number) {
+    if (confirm('Supprimer cet abonné ?')) {
+      this.api.deleteSubscriber(id).subscribe(() => this.loadSubscribers())
+    }
+  }
+
+  exportCsv() {
+    const rows = [['Téléphone', 'Email', 'Villes', 'Date inscription']]
+    for (const sub of this.subscribers) {
+      rows.push([
+        sub.phone,
+        sub.email,
+        sub.cities?.map((c: any) => c.name).join(' | ') || '',
+        new Date(sub.createdAt).toLocaleDateString('fr-FR'),
+      ])
+    }
+    const csv = rows.map((r) => r.join(',')).join('\n')
+    const blob = new Blob([csv], { type: 'text/csv' })
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = 'abonnes.csv'
+    a.click()
+  }
+}
