98b14cb55a1fb46cad47906731629b4da4b87809
qrulleau <quentin.rulleau@yahoo.com>
2026-05-12 16:10:41 +0200

    feat: add products admin page with CRUD

--- front/src/app/features/admin/pages/products/products.component.ts
+++ front/src/app/features/admin/pages/products/products.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import { ApiService } from '../../../../core/services/api.service'
 
 @Component({
-  selector: 'app-productscomponent',
+  selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
-  template: '<p>ProductsComponent works</p>',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './products.component.html',
 })
-export class ProductsComponent {}
+export class ProductsComponent implements OnInit {
+  products: any[] = []
+  showForm = false
+  editingProduct: any = null
+
+  form = {
+    name: '',
+    description: '',
+    price: '',
+    imageUrl: '',
+  }
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.loadProducts()
+  }
+
+  loadProducts() {
+    this.api.getProducts().subscribe((data) => (this.products = data))
+  }
+
+  openForm(product?: any) {
+    if (product) {
+      this.editingProduct = product
+      this.form = { ...product }
+    } else {
+      this.editingProduct = null
+      this.form = { name: '', description: '', price: '', imageUrl: '' }
+    }
+    this.showForm = true
+  }
+
+  closeForm() {
+    this.showForm = false
+    this.editingProduct = null
+  }
+
+  submit() {
+    if (this.editingProduct) {
+      this.api.updateProduct(this.editingProduct.id, this.form).subscribe(() => {
+        this.loadProducts()
+        this.closeForm()
+      })
+    } else {
+      this.api.createProduct(this.form).subscribe(() => {
+        this.loadProducts()
+        this.closeForm()
+      })
+    }
+  }
+
+  delete(id: number) {
+    if (confirm('Supprimer ce produit ?')) {
+      this.api.deleteProduct(id).subscribe(() => this.loadProducts())
+    }
+  }
+}
