5da729e4ccbe0db6c5cbea7d8ace9a152270a48d
qrulleau <quentin.rulleau@yahoo.com>
2026-05-12 16:24:03 +0200

    feat: add sellings places admin page with CRUD

--- front/src/app/features/admin/pages/selling-places/selling-places.component.ts
+++ front/src/app/features/admin/pages/selling-places/selling-places.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import { ApiService } from '../../../../core/services/api.service'
 
 @Component({
-  selector: 'app-sellingplacescomponent',
+  selector: 'app-selling-places',
   standalone: true,
-  imports: [CommonModule],
-  template: '<p>SellingPlacesComponent works</p>',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './selling-places.component.html',
 })
-export class SellingPlacesComponent {}
+export class SellingPlacesComponent implements OnInit {
+  sellingPlaces: any[] = []
+  showForm = false
+  editingPlace: any = null
+
+  form = {
+    name: '',
+    address: '',
+    schedule: '',
+  }
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.loadSellingPlaces()
+  }
+
+  loadSellingPlaces() {
+    this.api.getSellingPlaces().subscribe((data) => (this.sellingPlaces = data))
+  }
+
+  openForm(place?: any) {
+    if (place) {
+      this.editingPlace = place
+      this.form = { ...place }
+    } else {
+      this.editingPlace = null
+      this.form = { name: '', address: '', schedule: '' }
+    }
+    this.showForm = true
+  }
+
+  closeForm() {
+    this.showForm = false
+    this.editingPlace = null
+  }
+
+  submit() {
+    if (this.editingPlace) {
+      this.api.updateSellingPlace(this.editingPlace.id, this.form).subscribe(() => {
+        this.loadSellingPlaces()
+        this.closeForm()
+      })
+    } else {
+      this.api.createSellingPlace(this.form).subscribe(() => {
+        this.loadSellingPlaces()
+        this.closeForm()
+      })
+    }
+  }
+
+  delete(id: number) {
+    if (confirm('Supprimer ce lieu de vente ?')) {
+      this.api.deleteSellingPlace(id).subscribe(() => this.loadSellingPlaces())
+    }
+  }
+}
